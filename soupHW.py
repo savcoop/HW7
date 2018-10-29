59b2d6fc3747283ad1b787e33acef5383155c6ec
savcoop <savcoop@umich.edu>
2018-10-29 19:47:42 -0400

    final function

--- soupHW.py
+++ soupHW.py
@@ -44,11 +44,31 @@ def followLinks(url, numAnchor, numTimes):
     
 
 def getGradeHistogram(url):
-    """ return a sorted tuple with the grade range (such as 90, 80, etc) and the number of grades in that range
-        url -- a uniform resource locator - address for a web page
-    """
+	ctx = ssl.create_default_context()
+	ctx.check_hostname = False 
+	ctx.verify_mode = ssl.CERT_NONE
 
-    pass
+	html = urlopen(url, context =ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	span_tags = soup.find_all('span')
+	grades = {} 
+	for n in span_tags:
+		nums = re.findall(r'\b\d+\b', n.text)
+		for x in nums:
+			if len(x) > 1: 
+				g_range = int(x[0]) * 10
+			else: 
+				g_range = 0 
+
+			if g_range not in grades:
+				grades[g_range] = 1 
+			else: 
+				grades[g_range] += 1 
+
+	t_list = sorted(list(grades.items()), reverse = True)
+	return t_list
+
+    
 
 
 class TestHW7(unittest.TestCase):
@@ -65,8 +85,8 @@ class TestHW7(unittest.TestCase):
     def test_followLinks2(self):
         self.assertEqual(followLinks("http://py4e-data.dr-chuck.net/known_by_Charlie.html",18,7), "Shannah")
 
-    #def test_getGradeHistogram(self):
-    #    self.assertEqual(getGradeHistogram("http://py4e-data.dr-chuck.net/comments_42.html"), [(90, 4), (80, 4), (70, 7), (60, 7), (50, 6), (40, 3), (30, 5), (20, 4), (10, 6), (0, 4)])
+    def test_getGradeHistogram(self):
+       self.assertEqual(getGradeHistogram("http://py4e-data.dr-chuck.net/comments_42.html"), [(90, 4), (80, 4), (70, 7), (60, 7), (50, 6), (40, 3), (30, 5), (20, 4), (10, 6), (0, 4)])
 
 
 unittest.main(verbosity=2)
